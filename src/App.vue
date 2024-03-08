97b1e7fd1b4cf0d0d852274558805e1afc055bcc
Roman Fomichev <romanfomic@gmail.com>
2024-03-08 17:13:27 +0300

    Add basic structure and styles

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrapper">
+        <h1>Погода в вашем городе</h1>
+        <input type="text" placeholder="Введите город...">
+        <button>Получить</button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
+    
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    .wrapper {
+        padding: 200px;
+        border-radius: 50px;
+        box-shadow: 0 0 2px;
+    }
+    .wrapper h1 {
+        margin: 10px 0;
+    }
+    .wrapper input{
+        border: 0;
+        background: transparent;
+        font-size: 14px;
+        border-bottom: 2px solid deepskyblue;
+        padding: 5px;
+        outline: none;
+    }
+    .wrapper button {
+        border: 0;
+        background-color: deepskyblue;
+        color: white;
+        border-radius: 10px;
+        padding: 8px;
+        margin: 0 10px;
+        transition: transform 0.3s ease;
+        cursor: pointer;
+    }
+    .wrapper button:hover{
+        transform: scale(1.1);
+    }
 </style>
